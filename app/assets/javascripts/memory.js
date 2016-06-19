84688bca789222aa1b7871c25931ccad62f2c198
Luis Rita <luismestrerita@gmail.com>
2016-06-19 19:40:01 +0100

    simple one step game mechanics

--- app/assets/javascripts/memory.js
+++ app/assets/javascripts/memory.js
@@ -2,25 +2,35 @@ $(document).ready( function(){
   startGame();
 });
 
+var randNumber, rightAnswer, maxSteps = 10, currentStep;
+
 function generateRandomNumber() {
-  var randNumber = Math.floor(Math.random() * 1001),
-      numberField = document.getElementsByClassName('js-randNumber')[0];
+  var numberField = document.getElementsByClassName('js-randNumber')[0];
+  randNumber = Math.floor(Math.random()*90000) + 10000;
 
   numberField.innerHTML = randNumber;
+
+  return randNumber;
 }
 
 function hideNumber(time) {
   setTimeout(function(){
     var numberField = document.getElementsByClassName('numbers-container__number')[0],
-        playerInput = document.getElementById('player-input');
+        playerInput = document.getElementsByClassName('numbers-container__input')[0];
 
     numberField.className += " hide";
     playerInput.className += " active";
 
-
   }, time)
   
-  $('.progress-bar span').animate({width: "100%"}, time, "linear");
+  $('.progress-bar span').animate(
+    {width: "100%"}, 
+    time, 
+    "linear",
+    function() {
+      $(this).css('width', '0');
+    }
+  );
 }
 
 function startGame() {
@@ -29,9 +39,59 @@ function startGame() {
       $('.numbers-container').fadeIn();
     });
 
-    time = 5000;
+    rightAnswer = 0
+    currentStep = 1;
+
+    gameProgression();
+  });
+}
+
+function checkNumbersMatch() {
+  var submit = document.getElementsByClassName('js-submit-number')[0];
+
+  submit.addEventListener('click', comparePlayerInput);
+
+  currentStep++;
+}
+
+function comparePlayerInput() {
+  var inputVal = parseInt(document.getElementById('player-input').value, 10);
+
+  if(randNumber === inputVal) {
+    console.log('success!');
+
+    rightAnswer++;
+
+  } else {
+    console.log('aids');
+
+    wrongAnswer++;
+  }
+
+  setTimeout(gameProgression, 2000);
+}
+
+function resetNumber() {
+  document.getElementById('player-input').value = "";
+  var numberField = document.getElementsByClassName('numbers-container__number')[0],
+      playerInput = document.getElementsByClassName('numbers-container__input')[0];
+
+  numberField.className = "numbers-container__number";
+  playerInput.className = "numbers-container__input";
+}
+
+function gameProgression() {
+  var time = 5000;
+
+  if (currentStep < maxSteps) {
+    if(currentStep > 1) {
+      resetNumber();
+    }
 
     generateRandomNumber();
     hideNumber(time);
-  });
+    checkNumbersMatch();
+  } else {
+    console.log('You got ' + rightAnswer + ' in ' + maxSteps);
+  }
 }
